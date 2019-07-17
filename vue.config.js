d38c173eec7ba3f0536550c1cad6c833f13323aa
Ken Huang <kenhuang.au@gmail.com>
2019-07-17 14:22:01 +1000

    Add `npm run deploy` for Github Pages Deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const globby = require('globby')
 const PurgecssPlugin = require('purgecss-webpack-plugin')
+const { name } = require('./package.json')
 
 module.exports = {
+  // https://cli.vuejs.org/guide/deployment.html#github-pages
+  publicPath: process.env.NODE_ENV === 'production'
+    ? `/${name}/`
+    : '/',
+
   configureWebpack: {
     plugins: [
       // https://github.com/FullHuman/purgecss/tree/master/examples/with-vue
